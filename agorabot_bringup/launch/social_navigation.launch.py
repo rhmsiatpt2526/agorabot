7d8c92308175280130855deaa898bb6bc3d30521
Rémi Hamon <remi.hamon2022@gmail.com>
2026-05-27 16:09:05 +0200

    Ajout gitignore et deux autres fichiers

--- agorabot_bringup/launch/social_navigation.launch.py
+++ agorabot_bringup/launch/social_navigation.launch.py
@@ -2,10 +2,7 @@ from launch import LaunchDescription
 
 from launch_ros.actions import Node
 
-from launch.actions import (
-    IncludeLaunchDescription,
-    DeclareLaunchArgument
-)
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
@@ -23,12 +20,16 @@ def generate_launch_description():
     #
 
     scenario_arg = DeclareLaunchArgument(
-        'scenario',
-        default_value='agents_house.yaml',
-        description='HuNav scenario YAML file'
+        "scenario",
+        default_value="agents_house.yaml",
+        description="HuNav scenario YAML file",
     )
 
-    scenario = LaunchConfiguration('scenario')
+    scenario = LaunchConfiguration("scenario")
+
+    #
+    # RViz config
+    #
 
     #
     # HuNav launch
@@ -37,15 +38,12 @@ def generate_launch_description():
     hunav_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(
-                get_package_share_directory('hunav_gazebo_wrapper'),
-                'launch',
-                'small_house.launch.py'
+                get_package_share_directory("hunav_gazebo_wrapper"),
+                "launch",
+                "small_house.launch.py",
             )
         ),
-
-        launch_arguments={
-            'configuration_file': scenario
-        }.items()
+        launch_arguments={"configuration_file": scenario}.items(),
     )
 
     #
@@ -53,10 +51,10 @@ def generate_launch_description():
     #
 
     human_markers_node = Node(
-        package='agorabot_social_layer',
-        executable='human_markers_node',
-        name='human_markers_node',
-        output='screen'
+        package="agorabot_social_layer",
+        executable="human_markers_node",
+        name="human_markers_node",
+        output="screen",
     )
 
     #
@@ -64,10 +62,10 @@ def generate_launch_description():
     #
 
     social_costmap_node = Node(
-        package='agorabot_social_layer',
-        executable='social_costmap_node',
-        name='social_costmap_node',
-        output='screen'
+        package="agorabot_social_layer",
+        executable="social_costmap_node",
+        name="social_costmap_node",
+        output="screen",
     )
 
     #
@@ -75,10 +73,10 @@ def generate_launch_description():
     #
 
     social_behavior_node = Node(
-        package='agorabot_social_layer',
-        executable='social_behavior_node',
-        name='social_behavior_node',
-        output='screen'
+        package="agorabot_social_layer",
+        executable="social_behavior_node",
+        name="social_behavior_node",
+        output="screen",
     )
 
     #
@@ -86,18 +84,20 @@ def generate_launch_description():
     #
 
     keepout_filter_mask_server = Node(
-        package='nav2_map_server',
-        executable='map_server',
-        name='keepout_filter_mask_server',
-        output='screen',
-        parameters=[{
-            'yaml_filename': os.path.join(
-                os.path.expanduser('~/ros2_ws/src/agorabot/maps'),
-                'social_keepout_mask.yaml'
-            ),
-            'topic_name': '/keepout_filter_mask',
-            'frame_id': 'map'
-        }]
+        package="nav2_map_server",
+        executable="map_server",
+        name="keepout_filter_mask_server",
+        output="screen",
+        parameters=[
+            {
+                "yaml_filename": os.path.join(
+                    os.path.expanduser("~/ros2_ws/src/agorabot/maps"),
+                    "social_keepout_mask.yaml",
+                ),
+                "topic_name": "/keepout_filter_mask",
+                "frame_id": "map",
+            }
+        ],
     )
 
     #
@@ -105,54 +105,55 @@ def generate_launch_description():
     #
 
     costmap_filter_info_server = Node(
-        package='nav2_map_server',
-        executable='costmap_filter_info_server',
-        name='costmap_filter_info_server',
-        output='screen',
-        parameters=[{
-            'type': 0,
-            'filter_info_topic': '/costmap_filter_info',
-            'mask_topic': '/keepout_filter_mask',
-            'base': 0.0,
-            'multiplier': 1.0
-        }]
+        package="nav2_map_server",
+        executable="costmap_filter_info_server",
+        name="costmap_filter_info_server",
+        output="screen",
+        parameters=[
+            {
+                "type": 0,
+                "filter_info_topic": "/costmap_filter_info",
+                "mask_topic": "/keepout_filter_mask",
+                "base": 0.0,
+                "multiplier": 1.0,
+            }
+        ],
     )
 
+    #
+    # Lifecycle manager
+    #
 
     lifecycle_manager_filters = Node(
-        package='nav2_lifecycle_manager',
-        executable='lifecycle_manager',
-        name='lifecycle_manager_costmap_filters',
-        output='screen',
-        parameters=[{
-            'use_sim_time': True,
-            'autostart': True,
-            'node_names': [
-                'keepout_filter_mask_server',
-                'costmap_filter_info_server'
-            ]
-        }]
+        package="nav2_lifecycle_manager",
+        executable="lifecycle_manager",
+        name="lifecycle_manager_costmap_filters",
+        output="screen",
+        parameters=[
+            {
+                "use_sim_time": True,
+                "autostart": True,
+                "node_names": [
+                    "keepout_filter_mask_server",
+                    "costmap_filter_info_server",
+                ],
+            }
+        ],
     )
 
     #
     # Launch everything
     #
 
-    return LaunchDescription([
-
-        scenario_arg,
-
-        hunav_launch,
-
-        human_markers_node,
-
-        social_costmap_node,
-
-        social_behavior_node,
-
-        keepout_filter_mask_server,
-
-        costmap_filter_info_server,
-
-        lifecycle_manager_filters
-    ])
+    return LaunchDescription(
+        [
+            scenario_arg,
+            hunav_launch,
+            human_markers_node,
+            social_costmap_node,
+            social_behavior_node,
+            keepout_filter_mask_server,
+            costmap_filter_info_server,
+            lifecycle_manager_filters,
+        ]
+    )
